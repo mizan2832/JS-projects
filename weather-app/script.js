90b06c86c0e5ed392d31c0b105acb4448f3ba344
Mizanur Rahman <46856544+mizan2832@users.noreply.github.com>
2026-01-06 23:52:49 +0600

    JS add

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,12 +3,20 @@ const API_KEY = "ed26bfc5d42fd86e72deeb1a052b15d9";
 document.getElementById("districtSelect").addEventListener("change",function () {
     const city = this.value;
     if(!city) return;
+    let dateTime = new Date();
+    document.querySelector(".datetime").innerHTML = `${city} . ${dateTime.getMonth()} ${dateTime.getDay()},${dateTime.getFullYear()}`;
 
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city},BD&units=metric&appid=${API_KEY}`;
     fetch(API_URL)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+
+            document.querySelector(".temperature").innerHTML =
+                `${Math.round(data.main.temp)}<span>°C</span>`;
+
+            document.querySelector(".condition").innerText =
+                data.weather[0].description;
+                
             
         })
 
