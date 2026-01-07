59fdf01660a2ae05c0ea34832cec42a7de3364c1
Mizanur Rahman <46856544+mizan2832@users.noreply.github.com>
2026-01-07 23:33:24 +0600

    sunrise

--- weather-app/script.js
+++ weather-app/script.js
@@ -10,28 +10,34 @@ document.getElementById("districtSelect").addEventListener("change",function () 
     // Array of month names
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    // Format hours in 12-hour format with AM/PM
     let hours = dateTime.getHours();
     const minutes = dateTime.getMinutes().toString().padStart(2, "0");
     const ampm = hours >= 12 ? "PM" : "AM";
     hours = hours % 12;
-    hours = hours ? hours : 12; // convert 0 to 12
+    hours = hours ? hours : 12; 
+
+    const timezoneOffset = 21600;
+
+   
 
     // Build formatted string
     const formatted = `${city} • ${months[dateTime.getMonth()]} ${dateTime.getDate()}, ${dateTime.getFullYear()} • ${hours}:${minutes} ${ampm}`;
-
     document.querySelector(".datetime").innerHTML = formatted;
-
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city},BD&units=metric&appid=${API_KEY}`;
     fetch(API_URL)
         .then(res => res.json())
         .then(data => {
-
+            console.log(data);
             document.querySelector(".temperature").innerHTML =
                 `${Math.round(data.main.temp)}<span>°C</span>`;
 
             document.querySelector(".condition").innerText =
                 data.weather[0].description;
+
+            const sunriseLocal = new Date((data.sys.sunrise + timezoneOffset) * 1000);
+            const sunsetLocal  = new Date((data.sys.sunrise + timezoneOffset) * 1000);
+
+            console.log(sunriseLocal);
                 
             
         })
